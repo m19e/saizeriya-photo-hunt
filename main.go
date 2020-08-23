e7efdeafed34b19f6558af8cb7d3f85ede31a9d0
m19e <nexus.me7290@gmail.com>
2020-08-24 02:06:45 +0900

    Crop processing area

--- main.go
+++ main.go
@@ -1,27 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"image"
+	"image/color"
+	"image/jpeg"
 	"image/png"
 	"log"
 	"os"
 )
 
-func main() {
-	file, err := os.Open("./ref.png")
+func crop(fp string) {
+	file, err := os.Open(fp)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	img, err := png.Decode(file)
+	img, err := jpeg.Decode(file)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	imgBounds := img.Bounds()
+
+	originR := color.RGBAModel.Convert(img.At(imgBounds.Min.X, imgBounds.Min.Y)).(color.RGBA).R
+	leftBorder := 0
+	for l := imgBounds.Min.X; l < 100; l++ {
+		c := color.RGBAModel.Convert(img.At(l, imgBounds.Min.Y))
+		cR := c.(color.RGBA).R
+		if cR < originR-5 {
+			leftBorder = l
+			fmt.Println("left border", leftBorder)
+			break
+		}
+	}
+
+	rightBorder := 0
+	for r := 1; r < 100; r++ {
+		co := color.RGBAModel.Convert(img.At(imgBounds.Max.X-r, imgBounds.Min.Y))
+		coR := co.(color.RGBA).R
+		if coR < originR-5 {
+			rightBorder = r - 1
+			fmt.Println("right border", rightBorder)
+			break
+		}
+	}
+
 	subimg := img.(interface {
 		SubImage(r image.Rectangle) image.Image
-	}).SubImage(image.Rect(30, 0, img.Bounds().Dx(), img.Bounds().Dy()))
+	}).SubImage(image.Rect(leftBorder, 0, imgBounds.Max.X-rightBorder, imgBounds.Max.Y))
 
 	output, outputErr := os.Create("output.png")
 	if outputErr != nil {
@@ -29,4 +57,10 @@ func main() {
 	}
 	defer output.Close()
 	png.Encode(output, subimg)
+
+	fmt.Println(subimg.Bounds().Max.X, subimg.Bounds().Min.X)
+}
+
+func main() {
+	crop("./body.jpg")
 }
